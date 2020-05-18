9e989c98ec0c87c9e8b42176c817260fda1a17dd
aaaaa <amikusanta@gmail.com>
2020-05-18 10:25:55 +0900

    firebaseにデーター保存完了　一旦完成

--- src/js/vue.js
+++ src/js/vue.js
@@ -5,10 +5,11 @@ Vue.component('board-list', {
     template: `
         <div class="c-board--container">
             <h2 class="c-board--container__name">{{name}}</h2>
-            <p class="c-board--container__text">{{text}} {{date}}</p>
+            <p class="c-board--container__text">{{text}}</p>
+            <p class="c-board--container__date">{{date}}</p>
         </div>
     `,
-    props: ['name', 'text','date']
+    props: ['name','text','date']
 })
 
 //親
@@ -17,7 +18,7 @@ Vue.component('board-form', {
         <div class="c-modal">
             <button class="c-modal__btn c-modal__btn--cancel js-cancel">キャンセル</button>
             <button class="c-modal__btn c-modal__btn--send js-cancel" v-on:click="send">送信</button>
-            <input type="text" class="c-modal__name" placeholder="名前" v-model="name" ref="name">
+            <input type="text" class="c-modal__name" placeholder="名前" v-model="name">
             <textarea class="c-modal__textarea" placeholder="何か書いてみよう！" v-model="text"></textarea>
         </div>
     `,
@@ -29,8 +30,10 @@ Vue.component('board-form', {
     },
     methods:{
         send:function(){
-            //イベント発火
+            //イベント発火　名前とテキストを渡す　送信後フォーム削除
             this.$emit('input',this.name,this.text)
+            this.name =  ''
+            this.text = ''
         }
     }
 })
@@ -38,20 +41,24 @@ Vue.component('board-form', {
 let board = new Vue({
     el:'#l-board',
     data:{
-        name:'',
-        text:'',
-        data:'',
         lists:[]
     },
+    //firebaseからデータ取得
+    created: function(){
+        let vue = this;
+        firebase.database().ref('board').on('value', function(snapshot) {
+            vue.lists = snapshot.val();
+        });
+    },
+    //データ送信
     methods:{
         send:function(name,text){
             let now = new Date();
-            this.lists.push({
+            firebase.database().ref('board').push({
                 name:name,
                 text:text,
-                date:now.getMonth()+1 + '月'+ now.getDate() + '日' + now.getHours() + '時' + now.getMinutes() + '分'
+                date:now.getMonth()+1 + '月'+ now.getDate() + '日' + now.getHours() + ':' + now.getMinutes()
             })
         }
     }
 })
-
